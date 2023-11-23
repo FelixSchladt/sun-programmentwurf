d997d5e308117cdf654ea0365b271b2bf23aa7db
Felix Schladt <felix.schladt@gmail.com>
2023-11-23 10:29:57 +0100

    Website with sqli

--- search_jobs.php
+++ search_jobs.php
@@ -1,28 +1,81 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Job-Suche</title>
+    <title>Stellenanzeigen</title>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: white;
+            color: black;
+            margin: 0;
+            padding: 0;
+        }
+        .header, .footer {
+            background-color: #004d00; /* Dark Green */
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+        }
+        .container {
+            padding: 20px;
+        }
+        .search-form {
+            margin-bottom: 20px;
+        }
+        .job {
+            margin-bottom: 15px;
+            padding: 10px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+        .nav {
+            background-color: #009900; /* Light Green */
+            padding: 10px 0;
+            text-align: center;
+        }
+        .nav a {
+            color: white;
+            text-decoration: none;
+            padding: 10px 20px;
+        }
+        .nav a:hover {
+            background-color: #006600;
+        }
+ 
+    </style>
 </head>
 <body>
 
-<h1>Suche nach Jobtiteln</h1>
+<div class="header">
+    <h1>Stellenanzeigen</h1>
+</div>
 
-<!-- Suchformular -->
-<form method="post" action="search_jobs.php">
-    <input type="text" name="search">
-    <input type="submit" value="Suchen">
-</form>
+<div class="nav">
+    <a href="index.html">Startseite</a>
+    <a href="search_jobs.php">Jobs</a>
+    <a href="index.html#imprint">Impressum</a>
+</div>
 
-<?php
-// Überprüfen, ob eine Suchanfrage gesendet wurde
-if (isset($_POST['search'])) {
+
+<div class="container">
+    <div class="search-form">
+        <form method="post" action="search_jobs.php">
+            <input type="text" name="search" placeholder="Jobtitel suchen...">
+            <input type="submit" value="Suchen">
+        </form>
+    </div>
+
+    <?php
+    // Setzen Sie $searchTerm auf den gesendeten Wert oder auf einen leeren String
+    $searchTerm = isset($_POST['search']) ? $_POST['search'] : '';
+
     // Datenbankverbindung
-    $dbPath = 'pfad/zur/jobs.db';
+    $dbPath = 'jobs.db';
     $db = new SQLite3($dbPath);
 
-    $searchTerm = $_POST['search'];
-
-    // SQL-Injection-anfällige Abfrage
+    // Datenbankabfrage, die immer ausgeführt wird
     $query = "SELECT * FROM Jobs WHERE JobTitle LIKE '%$searchTerm%'";
 
     $results = $db->query($query);
@@ -30,12 +83,11 @@ if (isset($_POST['search'])) {
     // Ergebnisse anzeigen
     if ($results) {
         while ($row = $results->fetchArray()) {
-            echo "<div>";
-            echo "<strong>Job ID:</strong> " . htmlspecialchars($row['JobID']) . "<br>";
-            echo "<strong>Job Title:</strong> " . htmlspecialchars($row['JobTitle']) . "<br>";
-            echo "<strong>Description:</strong> " . htmlspecialchars($row['Description']) . "<br>";
-            echo "<strong>Employment Type:</strong> " . htmlspecialchars($row['EmploymentType']) . "<br>";
-            echo "</div><br>";
+            echo "<div class='job'>";
+            echo "<strong>Stelle:</strong> " . htmlspecialchars($row['JobTitle']) . "<br>";
+            echo "<strong>Beschreibung:</strong> " . htmlspecialchars($row['Description']) . "<br>";
+            echo "<strong>Stellentyp:</strong> " . htmlspecialchars($row['EmploymentType']) . "<br>";
+            echo "</div>";
         }
     } else {
         echo "Keine Jobs gefunden.";
@@ -43,8 +95,10 @@ if (isset($_POST['search'])) {
 
     // Schließen der Datenbankverbindung
     $db->close();
-}
-?>
-
+    ?>
+</div>
+<div class="footer">
+    <p>&copy; 2023 Wissensmanufaktur GmbH</p>
+</div>
 </body>
 </html>
